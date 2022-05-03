37f695784248c5e216a754bb1c3754737d6404a0
CodySawa <kota418@gmail.com>
2022-05-02 19:58:25 -0700

    save text in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,35 +13,45 @@ function generateTimeBlock(hour) {
     var colTime = $("<div>")
         .addClass("col-sm-2 hour")
         .text(convertTime(hour));
-    
+
     var colDescription = $("<div>")
         .addClass("col-sm-8 description");
 
     var descriptionInput = $("<textarea>")
+
     colDescription.append(descriptionInput);
     descriptionInput.attr("id", hour);
     if (hour < moment().format("HH")) {
-        descriptionInput.attr({"class": "past"});
+        descriptionInput.attr({ "class": "past" });
     } else if (hour == moment().format("HH")) {
-        descriptionInput.attr({"class": "present"});
+        descriptionInput.attr({ "class": "present" });
     } else if (hour > moment().format("HH")) {
-        descriptionInput.attr({"class": "future"});
+        descriptionInput.attr({ "class": "future" });
     }
 
     var saveBtn = $("<i class='fa-solid fa-floppy-disk'></i>")
     var save = $("<button>")
-        .attr({"class": "col-sm-2 saveBtn"});
+        .addClass("col-sm-2 saveBtn")
+        .attr("id", hour);
 
     save.append(saveBtn);
     row.append(colTime, colDescription, save);
-    
+
 }
 
 // display time blocks on index.html
 function DisplayTimeBlocks() {
     for (var i = 9; i <= 18; i++) {
         generateTimeBlock(i);
+        loadStorage(i);
     }
+}
+
+// load saved storage
+function loadStorage(hour) {
+    let inputval = localStorage.getItem(hour)
+    var text = $(`textarea#${hour}`).val(inputval)
+    
 }
 
 // converts military time to AM/PM
@@ -58,17 +68,24 @@ function convertTime(hour) {
     return hour + aORp;
 }
 
-setInterval (function() {
+setInterval(function () {
     for (var i = 9; i <= 18; i++) {
         if (i < moment().format("HH")) {
-            $(`#${i}`).attr({"class": "past"});
+            $(`#${i}`).attr({ "class": "past" });
         } else if (i == moment().format("HH")) {
-            $(`#${i}`).attr({"class": "present"});
+            $(`#${i}`).attr({ "class": "present" });
         } else if (i > moment().format("HH")) {
-            $(`#${i}`).attr({"class": "future"});
+            $(`#${i}`).attr({ "class": "future" });
         }
     }
 }, 1000)
 
 displayHeaderDate();
 DisplayTimeBlocks();
+
+$(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+    var id = this.id;
+    var inputText = document.getElementById(id).value;
+    localStorage.setItem(id, inputText);
+})
